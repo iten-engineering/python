efa8f76d3221f3fe0ee2a1fd7e418a0640b0a1f1
Tom <tom.iten@surfmachine.ch>
2022-11-07 09:49:32 +0100

    work

---
+++ handout/02-basic/demo.py
@@ -0,0 +1,17 @@
+
+# Prüfen ob Zahl 5 gerade ist
+x = 5 % 2
+print("5 % 2 = ", x)
+
+# Prüfen ob Zahl 6 gerade ist
+x = 6 % 2
+print("6 % 2 = ", x)
+
+# Boolean
+x = 10
+condition_1 = x > 5     # True
+condition_2 = False
+
+print (condition_1 or condition_2)
+print (condition_1 and condition_2 and True)
+
