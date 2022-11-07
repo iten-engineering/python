320c508133cac17449c1753186e5f51b049e95d5
Tom <tom.iten@surfmachine.ch>
2022-11-07 10:36:36 +0100

    work

---
+++ handout/02-basic/fahrenheit.py
@@ -0,0 +1,15 @@
+
+value = input("Temperatur in Fahrenheit:")
+# print(value)
+# print(type(value))
+
+fahrenheit = float(value)
+# print(fahrenheit)
+# print(type(fahrenheit))
+
+celsius = 5 * (fahrenheit - 32) / 9
+
+print("Temperatur:")
+print("Fahrenheit =", fahrenheit)
+print("Celsius    =", celsius)
+
