24481382652495af50cb07a92abdeadaff1346ad
Tom <tom.iten@surfmachine.ch>
2023-04-21 14:02:38 +0200

    work

--- handout/07-std-libs/re-demo.py
+++ handout/07-std-libs/re-demo.py
@@ -10,3 +10,17 @@ print(matches)  # Output: ['\t', '\n\n']
 text2 = re.sub(pattern, "-", text)
 print(text2)
 
+
+# E-Mail
+def is_valid_email(email):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.match(pattern, email) is not None
+
+tests = [
+    "x.y@test.com",
+    "x.y@test"
+]
+
+for test in tests:
+    result = is_valid_email(test)
+    print(test, result)
