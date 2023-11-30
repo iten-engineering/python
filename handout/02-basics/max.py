697270f84d62b4a3880cbeb7f54f1edd1f2ff0c9
Tom <tom.iten@surfmachine.ch>
2023-11-30 15:27:07 +0100

    work

---
+++ handout/02-basics/max.py
@@ -0,0 +1,18 @@
+def max(x, y, z=None):
+    if isinstance(z, type(None)):
+        return x if x > y else y
+    if x > y and x > z:
+        return x
+    elif y > z:
+        return y
+    return z
+
+
+print(max(1,4))
+
+def get_person():
+    return "Anna", 12, "Bern"
+
+result = get_person()
+print(result)
+
