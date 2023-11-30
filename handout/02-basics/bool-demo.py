f3344c7b639ee3836aa31d91c46e7f47286edf66
Tom <tom.iten@surfmachine.ch>
2023-11-30 10:35:55 +0100

    work

--- handout/02-basics/bool-demo.py
+++ handout/02-basics/bool-demo.py
@@ -7,3 +7,9 @@ has_alcohol = True
 
 is_allowed = is_adult and has_alcohol
 print(is_allowed)
+
+
+x = 5
+X = 7
+
+print("x=", x, ", X=", X)
---
+++ handout/02-basics/list-demo.py
@@ -0,0 +1,11 @@
+
+lotto = [12, 14]
+
+lotto.append(7)
+lotto.append(5)
+
+print(len(lotto))
+print(lotto)
+
+print(5 in lotto)
+print(-5 in lotto)
