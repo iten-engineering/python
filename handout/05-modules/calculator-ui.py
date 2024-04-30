3aa624316478b093cf28be5d31f037649240e1bb
Tom <tom.iten@surfmachine.ch>
2024-04-30 11:03:19 +0200

    work

--- handout/05-modules/calculator-ui.py
+++ handout/05-modules/calculator-ui.py
@@ -1,2 +1,20 @@
+# import calculator
+# calculator.add(1,2)
+
 from calculator import add, sub, mul, div
 
+data = input("Calculate = ")
+x, op, y = data.split()
+
+map = {"+":add, "-":sub, "*":mul, "/":div, "&":add,
+       "add": add}
+
+f = map.get(op)
+
+if f is None:
+    print(f"Unkown operator: {op}")
+elif op == "/" and y == "0":
+    print("Division by zero")
+else:
+    res = f(float(x),float(y))
+    print(res)
