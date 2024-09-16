6053caccf22531eecc7de537deebfb314ae5402b
Tom <tom.iten@surfmachine.ch>
2024-09-16 16:14:49 +0200

    work

--- handout/03-datatypes/i18n.py
+++ handout/03-datatypes/i18n.py
@@ -1,9 +1,27 @@
+import json
 
+"""
 i18n = {
     "de": "Guten morgen",
     "fr": "Bon jour",
     "it": "Buongiorno",
     "en": "Good morning"
 }
+"""
 
-name = input("Nma")
+i18n_file = "handout/03-datatypes/i18n.json"
+
+with open(i18n_file, 'r') as json_file:
+    i18n = json.load(json_file)
+
+print(i18n["de"])
+print(i18n["en"])
+
+name = input("Name = ")
+lang  = input("Sprachcode = ")
+
+key = lang if lang in i18n.keys() else "en"
+greeting = i18n[key]
+
+print(greeting)
+print(name)
