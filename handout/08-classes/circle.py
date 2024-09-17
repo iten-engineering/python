680270e2c0e4c08d48ee5d722cf88b42d4815d1c
Tom <tom.iten@surfmachine.ch>
2024-09-17 15:51:15 +0200

    work

---
+++ handout/08-classes/circle.py
@@ -0,0 +1,26 @@
+import math
+
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def __str__(self):
+        return f"Circle with radius:{self.radius}"
+    
+    def area(self):
+        return self.radius * 2 * math.pi
+
+    def circumference(self):
+        return self.radius * self.radius * math.pi
+
+    def print(self):
+        print("Kreis mit Radius:", self.radius)
+        print("- Umfang =", self.circumference())
+        print("- Fläche =", self.area())
+
+
+circles = [Circle(4), Circle(7), Circle(9)]
+for circle in circles:
+    circle.print()
+
