34d4c59b5ebc3a4ef066115df46e9f307226cd24
Tom <tom.iten@surfmachine.ch>
2024-11-25 10:43:30 +0100

    work

---
+++ handout/02-basics/rechteck.py
@@ -0,0 +1,14 @@
+length = 10
+width = 5
+
+size = 2 * (length + width)
+area = length * width
+
+# message = f"Rechteck mit Länge {length} und Breite {width}"
+# print(message)
+
+print("Rechteck mit Länge", length, "und Breite", width)
+print("- Umfang =", size)
+
+
+print("- Fläche =", area)
