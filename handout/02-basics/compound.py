1e60ef46eca409df18383f698ee9da98caeeee0b
Tom <tom.iten@surfmachine.ch>
2024-11-25 10:11:53 +0100

    work

---
+++ handout/02-basics/compound.py
@@ -0,0 +1,25 @@
+
+x = 5
+x += 1
+print(x)
+
+y = 55
+numbers = [1, 5, 7]
+print(y in numbers)
+
+z = (5 * (3 + 7)) * 2
+
+articles = {
+    4711: "Tastatur",
+    4800: "Bildschrim"
+}
+
+print(articles[4711])
+print(articles.keys())
+
+
+eingabe = input("Geben Sie bitte einen Text ein:")
+print(eingabe, type(eingabe))
+
+fahrenheit = float(eingabe)
+print(fahrenheit, type(fahrenheit))
