68e11dc67d204e3f2434a403ef85d14756910310
Tom <tom.iten@surfmachine.ch>
2025-04-22 10:59:54 +0200

    work

---
+++ handout/02-basics/fahrenheit.py
@@ -0,0 +1,17 @@
+
+value = input("Temperatur in Fahrenheit: ")
+
+fahrenheit = float(value)
+celcius = 5 * (fahrenheit-32) / 9
+
+print("Temperatur")
+print("- Fahrenheit:", fahrenheit)
+print("- Celsius   :", celcius)
+
+# Naming
+
+# Camelcase
+tempFahrenheit = 1
+
+# Snakecase
+temp_fahrenheit = 1
