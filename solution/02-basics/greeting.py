661b57e9349f08b4ebe85cec09db918d7b00ee5b
Tom <tom.iten@surfmachine.ch>
2025-08-06 12:39:17 +0200

    work

---
+++ solution/02-basics/greeting.py
@@ -0,0 +1,8 @@
+
+def hello(name="World"):
+    print("Hello", name)
+
+hello()
+hello("Zoé")
+
+
---
+++ solution/02-basics/linear.py
@@ -0,0 +1,7 @@
+
+f = lambda x: 1 + 0.5 * x
+
+for x in [-2,-1,0,1,2,3]:
+    print("y für x", x, "=", f(x))
+
+
