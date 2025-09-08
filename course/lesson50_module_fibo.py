149b32b286b7cbd3a399cd6ce5c4f46af303f7a8
Tom <tom.iten@surfmachine.ch>
2025-09-08 20:47:50 +0200

    work

---
+++ course/lesson50_module_fibo.py
@@ -0,0 +1,20 @@
+def print_fib(n):
+    """ write Fibonacci series up to n """
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+
+def fib(n):
+    """ return Fibonacci series up to n """
+    a, b = 0, 1
+    result = []
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+if __name__ == "__main__":
+    print_fib(100)
